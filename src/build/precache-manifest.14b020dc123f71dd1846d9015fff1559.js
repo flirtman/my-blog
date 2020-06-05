2a9589098efb535efc3b6ccbbc24fd049b278dc7
flirtman <iliasvinin@gmail.com>
2020-06-04 20:10:29 -0400

    first commit

---
+++ src/build/precache-manifest.14b020dc123f71dd1846d9015fff1559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bdc678d39c3e912d82cdb9f5ca4c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a887f2b892c3dca6a77c",
+    "url": "/static/css/main.b2b27fe3.chunk.css"
+  },
+  {
+    "revision": "d3a9d6c2bbfbf7c7f9e4",
+    "url": "/static/js/2.1899e5a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1899e5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a887f2b892c3dca6a77c",
+    "url": "/static/js/main.66f1cdf4.chunk.js"
+  },
+  {
+    "revision": "921ac687539ca31687de",
+    "url": "/static/js/runtime-main.7c2b1bab.js"
+  }
+]);
